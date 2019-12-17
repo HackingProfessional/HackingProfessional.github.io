e31703dc4d9c827368f4be9148fd4375a57869d2
Binary-Cha0s <39344293+hackingprofessional@users.noreply.github.com>
2019-12-17 17:26:18 -0500

    Test

--- tests/gm/js/main.js
+++ tests/gm/js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "November 03, 2019 06:00:00",
+	date: "December 18, 2019 06:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
